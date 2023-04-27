1f0f5ff526b8baec27b30f39ec574d732d0ae9c5
Mike Tsypan <miket@victorytransport.com>
2023-04-27 08:03:36 -0400

    started adding shape classes

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const inquirer = require('inquirer');
+const {Square, Triangle, Circle} = require("./shapes.js");
 
 const questions = [
     {
       type: 'list',
       name: 'name',
       message: 'Pick a shape for your logo:',
+      choices: ["Square", "Circle", "Triangle"]
     },
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Please enter up to 3 characters for your logo',
+    },
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What background color would you like?',
+    },
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What text color would you like?',
+    },
+
 
 ]
