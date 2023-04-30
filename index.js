44d6db97070a91ec99acb77fe617f3d0d960aa22
Mike Tsypan <miket@victorytransport.com>
2023-04-30 08:14:21 -0400

    had all questions/answeres captured but later had my code crash and not run. need to figre it out

--- index.js
+++ index.js
@@ -1,28 +1,50 @@
 const inquirer = require('inquirer');
-const {Square, Triangle, Circle} = require("./shapes.js");
+const { Square, Triangle, Circle } = require("./lib/shapes.js");
+
 
 const questions = [
-    {
-      type: 'list',
-      name: 'name',
-      message: 'Pick a shape for your logo:',
-      choices: ["Square", "Circle", "Triangle"]
-    },
-    {
-      type: 'input',
-      name: 'name',
-      message: 'Please enter up to 3 characters for your logo',
-    },
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What background color would you like?',
-    },
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What text color would you like?',
-    },
+  {
+    type: 'list',
+    name: 'shape',
+    message: 'Pick a shape for your logo:',
+    choices: ["Square", "Circle", "Triangle"]
+  },
+  {
+    type: 'input',
+    name: 'text',
+    message: 'Enter up to 3 characters for your logo',
+  },
+  {
+    type: 'input',
+    name: 'backgroundColor',
+    message: 'What background color would you like?',
+  },
+  {
+    type: 'input',
+    name: 'textColor',
+    message: 'What text color would you like?',
+  },
 
 
 ]
+
+function init() {
+  inquirer.prompt(questions)
+    .then(answers => {
+      if (answers.shape === "Square") {
+        let shapeSquare = new Square(answers);
+        return shapeSquare.render()
+      }
+      console.log(answers)
+      if (answers.shape === "Circle") {
+        let shapeCircle = new Circle(answers);
+        return shapeCircle.render()
+      }
+      if (answers.shape === "Triangle") {
+        let shapeTriangle = new Triangle(answers);
+        return shapeTriangle.render()
+      }
+    })
+}
+
+init();
